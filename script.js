dc47051498e1e7e35f39d44a11e9a35d84025837
Jawid <jawidforotan100@gmail.com>
2022-01-10 10:28:42 +0100

    The document title and background color is changed and displayed in console

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log(document.title);
+console.log(document.body);
+document.title = "Modifying the DOM";
+console.log(document.title);
+document.body.style.backgroundColor = "#FF69B4";
+console.log(document.body.style.backgroundColor);
+document.body.style.backgroundColor = "yellowGreen";
+console.log(document.body.style.backgroundColor);
