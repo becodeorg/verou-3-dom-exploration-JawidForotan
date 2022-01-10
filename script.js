62455922b190b259f60bd2efbb3758ef2d38fca4
Jawid <jawidforotan100@gmail.com>
2022-01-10 11:04:57 +0100

    All the body children displayed in a loop

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-console.log(document.title);
+console.log(`title: ${document.title}`);
 console.log(document.body);
 document.title = "Modifying the DOM";
-console.log(document.title);
+console.log(`new title: ${document.title}`);
 document.body.style.backgroundColor = "#FF69B4";
-console.log(document.body.style.backgroundColor);
+console.log(`background: ${document.body.style.backgroundColor}`);
 document.body.style.backgroundColor = "yellowGreen";
-console.log(document.body.style.backgroundColor);
+console.log(`new background ${document.body.style.backgroundColor}`);
+console.log(document.body.children);
+/*Loop through all children of the body*/
+let c = document.body.children;
+for(i = 0; i < c.length; i++){
+    console.log(c[i]);
+}
