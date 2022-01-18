1ae877d4998aed6ca1754bec44e3e415cc114c2e
Jawid <jawidforotan100@gmail.com>
2022-01-18 13:26:31 +0100

    Event exercise is finished

--- 5.Events/script.js
+++ 5.Events/script.js
@@ -4,29 +4,42 @@ function getElapsedTime() {
   return Number((Date.now() - _initTime) / 1000).toFixed(2) + "s";
 }
 
+function alertFunction() {
+  const selectDiv = document.querySelectorAll(".displayedSquare");
+  for (i = 0; i < selectDiv.length; i++) {
+    selectDiv[i].addEventListener("click", () => {
+      alert("The button shouldn't be clicked");
+    });
+  }
+}
+
 function clickOnSquare(e) {
-  console.log(e.target.classList[1]);
-  console.log(getElapsedTime());
+  const selectSec = document.querySelector(".displayedSquare-wrapper");
   const div = document.createElement("div");
   div.setAttribute("class", "displayedSquare");
-  const selectSec = document.querySelector(".displayedSquare-wrapper");
   selectSec.appendChild(div);
   const color = e.target.classList[1];
   div.classList.add(color);
- }
 
+  alertFunction();
+
+  const selectList = document.querySelector(".list");
+  const creatList = document.createElement("li");
+  selectList.appendChild(creatList);
+  creatList.innerHTML = getElapsedTime();
+
+  window.addEventListener("keydown", (e) => {
+    if (e.code === "Space") {
+      const randomColor = "#" + Math.floor(Math.random() * 5000);
+      document.body.style.background = randomColor;
+    } else if (e.code === "Backspace") {
+      selectList.remove();
+    } else if (e.key === "s") {
+      div.remove();
+    }
+  });
+}
 const actionSquares = document.querySelectorAll(".actionSquare-wrapper");
 for (let actionSquare of actionSquares) {
   actionSquare.addEventListener("click", clickOnSquare);
 }
-
-const selectMain = document.querySelector("main");
-
-const createH1 = document.createElement("h1");
-createH1.setAttribute("id", "myH1");
-selectMain.prepend(createH1);
-function myFunction(e) {
-  let unicode = e.which;
-  document.getElementById("myH1").innerHTML = unicode;
-}
-
